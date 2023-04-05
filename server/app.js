bce5767c48b996b41f01b571286ea908e18e8f0e
Boonraksa Katawundee <myboonraksa12@gmail.com>
2023-04-05 18:58:46 +0700

    finish

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const customerRoutes = require('./routes/customerRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
@@ -16,5 +18,7 @@ app.use(bodyParser.json());
 //http:localhost:8080/api/v1/customer/login
 //http:localhost:8080/api/v1/customer/signup
 app.use('/api/v1/customer', customerRoutes);
+app.use('/api/v1/product', productRoutes);
+app.use('/api/v1/order', orderRoutes);
 
 module.exports = app;              
